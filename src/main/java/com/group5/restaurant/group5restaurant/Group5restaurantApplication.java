a32ea949bbb534c9ee9f21f72c007a27264d14f3
Jonas324 <jonaswadin24@gmail.com>
2022-11-17 14:47:29 +0100

    first push

---
+++ src/main/java/com/group5/restaurant/group5restaurant/Group5restaurantApplication.java
@@ -0,0 +1,13 @@
+package com.group5.restaurant.group5restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group5restaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group5restaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/restaurant/group5restaurant/Group5restaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.restaurant.group5restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group5restaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
