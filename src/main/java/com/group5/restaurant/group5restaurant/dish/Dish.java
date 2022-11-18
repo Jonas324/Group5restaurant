b48202251ae6b775748d27b589f651db871e58f9
Jonathan <jonathanlindqvist@MacBook-Pro-som-tillhor-Jonathan.local>
2022-11-18 08:47:47 +0100

    add dish clas with connection to db table dishes

---
+++ src/main/java/com/group5/restaurant/group5restaurant/dish/Dish.java
@@ -0,0 +1,44 @@
+package com.group5.restaurant.group5restaurant.dish;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Dishes")
+public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "Dish")
+    private String dishName;
+    
+
+    public Dish() {
+    }
+
+    public Dish(String dishName) {
+        this.dishName = dishName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public void setDishName(String dishName) {
+        this.dishName = dishName;
+    }
+}
+
+
+
+
+
