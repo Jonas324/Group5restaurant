9e9ed59e1f03a1ab297de8953c4ad9ba252ba309
kambii <kambinho123@hotmail.com>
2022-11-20 01:50:15 +0100

    guest-klar

---
+++ src/main/java/com/group5/restaurant/group5restaurant/guest/Guest.java
@@ -0,0 +1,59 @@
+package com.group5.restaurant.group5restaurant.guest;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table
+public class Guest {
+
+    @Id
+    @SequenceGenerator(
+            name = "guest_sequence",
+            sequenceName = "guest_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "guest_sequence"
+    )
+    private Long id;
+    private String guestName;
+
+    public Guest() {
+    }
+
+    public Guest(Long id, String guestName) {
+        this.id = id;
+        this.guestName = guestName;
+    }
+
+    public Guest(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "id=" + id +
+                ", guestName='" + guestName + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/group5/restaurant/group5restaurant/guest/GuestRepository.java
@@ -0,0 +1,15 @@
+package com.group5.restaurant.group5restaurant.guest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+
+    @Query("SELECT s FROM Guest s WHERE s.guestName = ?1")
+    Optional<Guest> findGuestByName(String guestName);
+}
+
