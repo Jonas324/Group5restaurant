4b005999cdbcdcba98aa640816da45694d5395d0
ocmino <ocmino@gmail.com>
2022-11-21 15:48:39 +0100

    Add Booking controller, service and repository

---
+++ src/main/java/com/group5/restaurant/group5restaurant/booking/BookingController.java
@@ -0,0 +1,41 @@
+package com.group5.restaurant.group5restaurant.booking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/booking")
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping
+    public List<Booking> getBookings() {
+        return bookingService.getBookings();
+    }
+
+    @PostMapping
+    public void addBooking(@RequestBody Booking booking) {
+        bookingService.addBooking(booking);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateBooking(
+            @PathVariable("id") Integer id,
+            @RequestParam(required = false) String startTime,
+            @RequestParam(required = false) String endTime,
+            @RequestParam(required = false) Integer numberOfGuests,
+            @RequestParam(required = false) Integer tableID
+    ) {
+        bookingService.updateBooking(id, startTime, endTime, numberOfGuests, tableID);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBooking(@PathVariable Integer id) {
+        bookingService.deleteBooking(id);
+    }
+
+}
---
+++ src/main/java/com/group5/restaurant/group5restaurant/booking/BookingService.java
@@ -0,0 +1,45 @@
+package com.group5.restaurant.group5restaurant.booking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public List<Booking> getBookings() {
+        return bookingRepository.findAll();
+    }
+
+    public void addBooking(Booking booking) {
+        bookingRepository.save(booking);
+    }
+
+    public void updateBooking(Integer id, String startTime, String endTime, Integer numberOfGuests, Integer tableID) {
+        Booking booking = bookingRepository.findById(id).orElse(null);
+        if (booking != null) {
+            if (startTime != null) {
+                booking.setStartTime(startTime);
+            }
+            if (endTime != null) {
+                booking.setEndTime(endTime);
+            }
+            if (numberOfGuests != null) {
+                booking.setNumberOfGuests(numberOfGuests);
+            }
+            if (tableID != null) {
+                booking.setTableID(tableID);
+            }
+            bookingRepository.save(booking);
+        }
+    }
+
+    public void deleteBooking(Integer id) {
+        bookingRepository.deleteById(id);
+    }
+
+}
